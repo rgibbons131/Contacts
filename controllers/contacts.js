cfb1c931180a1a59ac5f049bc0b8e6f367364499
Richard Gibbons <richiegibbons99@gmail.com>
2023-04-28 20:53:29 -0600

    Removed debug logs

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,6 @@ const mongodb = require('../db/connect');
 const getData = async (req, res, next) => {
   const result = await mongodb.getDb().db('user').collection('contacts').find();
   result.toArray().then((lists) => {
-    console.log(lists);
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists); // we just need the first one (the only one)
   });
@@ -13,7 +12,7 @@ const getData = async (req, res, next) => {
 const getUser = async (req, res, next) => {
   const result = await mongodb.getDb().db('user').collection('contacts').findOne({"_id" :new ObjectId(req.params.userId)}).then( (account) => {
     if (account == null){
-      console.log("Account not found");
+      console.log("Account not found err");
     res.status(500).json({ message: "An error ocured" });
     }
     else{
